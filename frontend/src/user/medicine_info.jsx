5f4a6e7d8325caba78337dd204c9a2b466a16279
Mahesh <maheshmahi18042004@gmail.com>
2025-02-04 12:28:12 +0530

    kannada language

--- frontend/src/user/medicine_info.jsx
+++ frontend/src/user/medicine_info.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -52,10 +52,21 @@ const MedicineInfo = () => {
   };
 
   const [medicineName, setMedicineName] = useState(""); // Controlled input field
-  const [searchQuery, setSearchQuery] = useState(""); // Search query triggered on submit
   const [medicineResults, setMedicineResults] = useState([]); // Array to store search results
   const [error, setError] = useState(null);
   const [suggestions, setSuggestions] = useState([]); // Array to store pill name suggestions
+
+  // State for translated medicine details
+  const [translatedDetails, setTranslatedDetails] = useState({
+    dosage: "",
+    ageGroup: "",
+    sideEffects: "",
+    purpose: "",
+    warnings: "",
+    overdosage: "",
+    adverseActions: "",
+    generalPrecautions: "",
+  });
 
   // Helper function to get the position of the nth occurrence of a character
   const getNthOccurrence = (str, char, n) => {
@@ -74,6 +85,65 @@ const MedicineInfo = () => {
     return index !== -1 ? text.slice(0, index + 1) : text; // Include the 10th full stop
   };
 
+  // Function to translate text using the API
+  const translateText = async (text) => {
+    try {
+      const response = await axios.post(
+        "https://know-your-med-backend.vercel.app/translate",
+        {
+          text: text,
+        }
+      );
+      return response.data.translatedText;
+    } catch (err) {
+      console.error("Error translating text:", err);
+      return text; // Return original text if translation fails
+    }
+  };
+
+  // Function to translate all medicine details
+  const translateMedicineDetails = async (details) => {
+    const translated = {
+      dosage: await translateText(details.dosage),
+      ageGroup: await translateText(details.ageGroup),
+      sideEffects: await translateText(details.sideEffects),
+      purpose: await translateText(details.medicine?.purpose),
+      warnings: await translateText(details.medicine?.warnings),
+      overdosage: await translateText(details.medicine?.overdosage),
+      generalPrecautions: await translateText(
+        details.medicine?.general_precautions
+      ),
+    };
+    setTranslatedDetails(translated);
+  };
+
+  // Fetch medicine details and translate if language is Kannada
+  const fetchMedicineDetails = async (query) => {
+    try {
+      const response = await axios.get(
+        `https://know-your-med-backend.vercel.app/medicine/${query}`,
+        { withCredentials: true }
+      );
+      const details = response.data;
+
+      // Append new medicine details to the existing array
+      setMedicineResults((prevResults) => [
+        ...prevResults,
+        { name: query, details: details },
+      ]);
+
+      // Translate details if language is Kannada
+      if (language === "kn") {
+        await translateMedicineDetails(details);
+      }
+
+      setError(null); // Clear error on success
+    } catch (err) {
+      console.error("Error fetching medicine details:", err);
+      setError(err.response?.data?.message);
+    }
+  };
+
   const handleInputChange = (e) => {
     setMedicineName(e.target.value);
   };
@@ -86,27 +156,6 @@ const MedicineInfo = () => {
     fetchMedicineDetails(medicineName);
   };
 
-  const fetchMedicineDetails = async (query) => {
-    try {
-      // Fetch medicine details from backend API
-      const response = await axios.get(
-        `https://know-your-med-backend.vercel.app/medicine/${query}`,
-        { withCredentials: true }
-      );
-      console.log(response.data?.medicine);
-
-      // Append new medicine details to the existing array
-      setMedicineResults((prevResults) => [
-        ...prevResults,
-        { name: query, details: response.data },
-      ]);
-      setError(null); // Clear error on success
-    } catch (err) {
-      console.error("Error fetching medicine details:", err);
-      setError(err.response?.data?.message);
-    }
-  };
-
   const navigate = useNavigate();
   const handleLogout = () => {
     // Remove the session storage items
@@ -115,14 +164,21 @@ const MedicineInfo = () => {
     sessionStorage.removeItem("userId");
     sessionStorage.removeItem("email");
     sessionStorage.removeItem("language");
-    toast.info(translations[language].LogoutToast),
-      {
-        autoClose: 5000, // Toast will stay for 10 seconds
-      };
+    toast.info(translations[language].LogoutToast, {
+      autoClose: 5000, // Toast will stay for 5 seconds
+    });
     setTimeout(() => {
       navigate("/home");
     }, 5000);
   };
+
+  // Update translations when language changes
+  useEffect(() => {
+    if (language === "kn" && medicineResults.length > 0) {
+      const latestDetails = medicineResults[medicineResults.length - 1].details;
+      translateMedicineDetails(latestDetails);
+    }
+  }, [language, medicineResults]);
 
   return (
     <div className="medicine-info-container">
@@ -192,56 +248,64 @@ const MedicineInfo = () => {
           </h2>
           <p>
             <strong>{translations[language].DosageFormVal}</strong>{" "}
-            {sliceText(
-              medicineResults[medicineResults.length - 1].details.dosage
-            )}
+            {language === "kn"
+              ? sliceText(translatedDetails.dosage)
+              : sliceText(
+                  medicineResults[medicineResults.length - 1].details.dosage
+                )}
           </p>
           <p>
             <strong>{translations[language].AgeGrpVal}</strong>{" "}
-            {sliceText(
-              medicineResults[medicineResults.length - 1].details.ageGroup
-            )}
+            {language === "kn"
+              ? sliceText(translatedDetails.ageGroup)
+              : sliceText(
+                  medicineResults[medicineResults.length - 1].details.ageGroup
+                )}
           </p>
           <p>
             <strong>{translations[language].SideEffectsVal}</strong>{" "}
-            {sliceText(
-              medicineResults[medicineResults.length - 1].details.sideEffects
-            )}
+            {language === "kn"
+              ? sliceText(translatedDetails.sideEffects)
+              : sliceText(
+                  medicineResults[medicineResults.length - 1].details
+                    .sideEffects
+                )}
           </p>
           <p>
             <strong>{translations[language].PurposeVal}</strong>{" "}
-            {sliceText(
-              medicineResults[medicineResults.length - 1].details.medicine
-                ?.purpose
-            )}
+            {language === "kn"
+              ? sliceText(translatedDetails.purpose)
+              : sliceText(
+                  medicineResults[medicineResults.length - 1].details.medicine
+                    ?.purpose
+                )}
           </p>
           <p>
             <strong>{translations[language].WarningsVal}</strong>{" "}
-            {sliceText(
-              medicineResults[medicineResults.length - 1].details.medicine
-                ?.warnings
-            )}
+            {language === "kn"
+              ? sliceText(translatedDetails.warnings)
+              : sliceText(
+                  medicineResults[medicineResults.length - 1].details.medicine
+                    ?.warnings
+                )}
           </p>
           <p>
             <strong>{translations[language].OverdosageVal}</strong>{" "}
-            {sliceText(
-              medicineResults[medicineResults.length - 1].details.medicine
-                ?.overdosage
-            )}
-          </p>
-          <p>
-            <strong>{translations[language].AdverseActionsVal}</strong>{" "}
-            {sliceText(
-              medicineResults[medicineResults.length - 1].details.medicine
-                ?.adverse_reactions
-            )}
+            {language === "kn"
+              ? sliceText(translatedDetails.overdosage)
+              : sliceText(
+                  medicineResults[medicineResults.length - 1].details.medicine
+                    ?.overdosage
+                )}
           </p>
           <p>
             <strong>{translations[language].GeneralPrecautionsVal}</strong>{" "}
-            {sliceText(
-              medicineResults[medicineResults.length - 1].details.medicine
-                ?.general_precautions
-            )}
+            {language === "kn"
+              ? sliceText(translatedDetails.generalPrecautions)
+              : sliceText(
+                  medicineResults[medicineResults.length - 1].details.medicine
+                    ?.general_precautions
+                )}
           </p>
         </div>
       )}
@@ -251,3 +315,257 @@ const MedicineInfo = () => {
 };
 
 export default MedicineInfo;
+
+// import axios from "axios";
+// import { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import "./styles/medicine_info.css";
+
+// const MedicineInfo = () => {
+//   // Check sessionStorage for the language, default to 'en' if not found
+//   const [language, setLanguage] = useState(
+//     sessionStorage.getItem("language") || "en"
+//   );
+
+//   // Translations for different languages
+//   const translations = {
+//     en: {
+//       HomeNav: "Home",
+//       ContactUsNav: "Contact Us",
+//       LogOut: "Logout",
+//       SearchLbl: "Medicine Information Search",
+//       SearchBar: "Enter Medicine Name (example : Paracetamol)",
+//       ButtonVal: "Search",
+//       DetailsForVal: "Details for",
+//       DosageFormVal: "Dosage Form",
+//       AgeGrpVal: "Age Group",
+//       SideEffectsVal: "Side Effects",
+//       PurposeVal: "Purpose",
+//       WarningsVal: "Warnings",
+//       OverdosageVal: "Overdosage",
+//       AdverseActionsVal: "Adverse Actions",
+//       GeneralPrecautionsVal: "General Precautions",
+//       LogoutToast: "You have been logged out",
+//     },
+//     kn: {
+//       HomeNav: "ಹೋಮ್",
+//       ContactUsNav: "ನಮ್ಮನ್ನು ಸಂಪರ್ಕಿಸಿ",
+//       LogOut: "ಲಾಗ್ ಔಟ್",
+//       SearchLbl: "ಔಷಧಿ ಮಾಹಿತಿಯ ಹುಡುಕಾಟ",
+//       SearchBar: "ಔಷಧಿಯ ಹೆಸರು ನಮೂದಿಸಿ (ಉದಾಹರಣೆ: ಪ್ಯಾರಾಸಿಟಮೊಲ್)",
+//       ButtonVal: "ಹುಡುಕಿ",
+//       DetailsForVal: "ವಿವರಗಳು :",
+//       DosageFormVal: "ಡೋಸೇಜ್",
+//       AgeGrpVal: "ವಯೋಮಾನದ ಗುಂಪು",
+//       SideEffectsVal: "ಪರಿಣಾಮಗಳು",
+//       PurposeVal: "ಉದ್ದೇಶ",
+//       WarningsVal: "ಎಚ್ಚರಿಕೆಗಳು",
+//       OverdosageVal: "ಅತಿಮಾತ್ರೆ ಪರಿಣಾಮಗಳು",
+//       AdverseActionsVal: "ಅನನುಕೂಲಕಾರಿ ಕ್ರಿಯೆಗಳು",
+//       GeneralPrecautionsVal: "ಸಾಮಾನ್ಯ ಮುನ್ನೆಚ್ಚರಿಕೆಗಳು",
+//       LogoutToast: "ನೀವು ಲಾಗ್ ಔಟ್ ಆಗಿದ್ದೀರಿ",
+//     },
+//   };
+
+//   const [medicineName, setMedicineName] = useState(""); // Controlled input field
+//   const [searchQuery, setSearchQuery] = useState(""); // Search query triggered on submit
+//   const [medicineResults, setMedicineResults] = useState([]); // Array to store search results
+//   const [error, setError] = useState(null);
+//   const [suggestions, setSuggestions] = useState([]); // Array to store pill name suggestions
+
+//   // Helper function to get the position of the nth occurrence of a character
+//   const getNthOccurrence = (str, char, n) => {
+//     let index = -1;
+//     for (let i = 0; i < n; i++) {
+//       index = str.indexOf(char, index + 1);
+//       if (index === -1) break; // If fewer than n occurrences
+//     }
+//     return index;
+//   };
+
+//   // Function to slice text till the 10th full stop
+//   const sliceText = (text) => {
+//     if (!text) return "No information available";
+//     const index = getNthOccurrence(text, ".", 10);
+//     return index !== -1 ? text.slice(0, index + 1) : text; // Include the 10th full stop
+//   };
+
+//   const handleInputChange = (e) => {
+//     setMedicineName(e.target.value);
+//   };
+
+//   const handleSearch = () => {
+//     if (medicineName.trim() === "") {
+//       setError("Please Enter Valid Medicine Name");
+//       return;
+//     }
+//     fetchMedicineDetails(medicineName);
+//   };
+
+//   const fetchMedicineDetails = async (query) => {
+//     try {
+//       // Fetch medicine details from backend API
+//       const response = await axios.get(
+//         `https://know-your-med-backend.vercel.app/medicine/${query}`,
+//         { withCredentials: true }
+//       );
+//       console.log(response.data?.medicine);
+
+//       // Append new medicine details to the existing array
+//       setMedicineResults((prevResults) => [
+//         ...prevResults,
+//         { name: query, details: response.data },
+//       ]);
+//       setError(null); // Clear error on success
+//     } catch (err) {
+//       console.error("Error fetching medicine details:", err);
+//       setError(err.response?.data?.message);
+//     }
+//   };
+
+//   const navigate = useNavigate();
+//   const handleLogout = () => {
+//     // Remove the session storage items
+//     sessionStorage.removeItem("userName");
+//     sessionStorage.removeItem("userToken");
+//     sessionStorage.removeItem("userId");
+//     sessionStorage.removeItem("email");
+//     sessionStorage.removeItem("language");
+//     toast.info(translations[language].LogoutToast),
+//       {
+//         autoClose: 5000, // Toast will stay for 10 seconds
+//       };
+//     setTimeout(() => {
+//       navigate("/home");
+//     }, 5000);
+//   };
+
+//   return (
+//     <div className="medicine-info-container">
+//       {/* Navbar */}
+//       <div className="user-medicineinfo-navbar">
+//         {/* Logo */}
+//         <div className="nav-logo-container7">
+//           <img src={"/logo3.jpg"} alt="KYM Logo" />
+//         </div>
+//         <div className="nav-links-container-medicineinfo">
+//           <Link to="/user/home" className="nav-linkVal-container-medicineinfo">
+//             {translations[language].HomeNav}
+//           </Link>
+//           <Link to="/contactus" className="nav-linkVal-container-medicineinfo">
+//             {translations[language].ContactUsNav}
+//           </Link>
+//           <Link
+//             className="nav-linkVal-container-userhome"
+//             onClick={handleLogout}
+//           >
+//             {translations[language].LogOut}
+//           </Link>
+//         </div>
+//       </div>
+
+//       <h1 className="title-med-info">{translations[language].SearchLbl}</h1>
+
+//       {/* Input card */}
+//       <div className="input-card-medinfo">
+//         <input
+//           type="text"
+//           placeholder={translations[language].SearchBar}
+//           value={medicineName}
+//           onChange={handleInputChange}
+//         />
+//         {/* Dropdown for suggestions */}
+//         {suggestions.length > 0 && (
+//           <ul className="suggestions-dropdown-medinfo">
+//             {suggestions.map((suggestion, index) => (
+//               <li
+//                 key={index}
+//                 onClick={() => {
+//                   setMedicineName(suggestion); // Set selected suggestion in input field
+//                   setSuggestions([]); // Clear suggestions after selection
+//                 }}
+//               >
+//                 {suggestion}
+//               </li>
+//             ))}
+//           </ul>
+//         )}
+//         <div>
+//           <button onClick={handleSearch}>
+//             {translations[language].ButtonVal}
+//           </button>
+//         </div>
+//       </div>
+
+//       {error && <p style={{ color: "red" }}>{error}</p>}
+
+//       {/* Display latest searched medicine details */}
+//       {medicineResults.length > 0 && (
+//         <div className="medicine-card-medinfo">
+//           <h2>
+//             {translations[language].DetailsForVal.toUpperCase()}{" "}
+//             {medicineResults[medicineResults.length - 1].name.toUpperCase()}
+//           </h2>
+//           <p>
+//             <strong>{translations[language].DosageFormVal}</strong>{" "}
+//             {sliceText(
+//               medicineResults[medicineResults.length - 1].details.dosage
+//             )}
+//           </p>
+//           <p>
+//             <strong>{translations[language].AgeGrpVal}</strong>{" "}
+//             {sliceText(
+//               medicineResults[medicineResults.length - 1].details.ageGroup
+//             )}
+//           </p>
+//           <p>
+//             <strong>{translations[language].SideEffectsVal}</strong>{" "}
+//             {sliceText(
+//               medicineResults[medicineResults.length - 1].details.sideEffects
+//             )}
+//           </p>
+//           <p>
+//             <strong>{translations[language].PurposeVal}</strong>{" "}
+//             {sliceText(
+//               medicineResults[medicineResults.length - 1].details.medicine
+//                 ?.purpose
+//             )}
+//           </p>
+//           <p>
+//             <strong>{translations[language].WarningsVal}</strong>{" "}
+//             {sliceText(
+//               medicineResults[medicineResults.length - 1].details.medicine
+//                 ?.warnings
+//             )}
+//           </p>
+//           <p>
+//             <strong>{translations[language].OverdosageVal}</strong>{" "}
+//             {sliceText(
+//               medicineResults[medicineResults.length - 1].details.medicine
+//                 ?.overdosage
+//             )}
+//           </p>
+//           <p>
+//             <strong>{translations[language].AdverseActionsVal}</strong>{" "}
+//             {sliceText(
+//               medicineResults[medicineResults.length - 1].details.medicine
+//                 ?.adverse_reactions
+//             )}
+//           </p>
+//           <p>
+//             <strong>{translations[language].GeneralPrecautionsVal}</strong>{" "}
+//             {sliceText(
+//               medicineResults[medicineResults.length - 1].details.medicine
+//                 ?.general_precautions
+//             )}
+//           </p>
+//         </div>
+//       )}
+//       <ToastContainer />
+//     </div>
+//   );
+// };
+
+// export default MedicineInfo;
